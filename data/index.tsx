6771c4a6cce9cebbe9a61ebb796adf620b53f514
gerrad.natanael <gerrad.natanael@ui.ac.id>
2022-12-07 13:35:47 +0700

    Add projects, blogs, and nav items data

---
+++ data/index.tsx
@@ -0,0 +1,86 @@
+export const articles = [
+  {
+    url: 'https://medium.com/@gerrad.natanael/mock-testing-in-golang-203a37fdddd6',
+    img: '/assets/blog/mock-testing-golang.png',
+    title: 'Mock Testing in Golang'
+  },
+  {
+    url: 'https://medium.com/@gerrad.natanael/error-handling-in-wave-web-application-55a3297bb96b',
+    img: '/assets/blog/error-handling.jpg',
+    title: 'Error Handling in Wave Web Application'
+  },
+  {
+    url: 'https://medium.com/@gerrad.natanael/secure-user-authentication-with-bcrypt-2af2654111b7',
+    img: '/assets/blog/secure-user-auth.png',
+    title: 'Secure User Authentication with Bcrypt'
+  },
+  {
+    url: 'https://medium.com/@gerrad.natanael/refactoring-how-to-improve-your-code-1455747d0fce',
+    img: '/assets/blog/refactoring.png',
+    title: 'Refactoring: How to Improve your Code'
+  },
+  {
+    url: 'https://medium.com/@gerrad.natanael/nielsens-10-usability-heuristics-9ee7bb7f908f',
+    img: '/assets/blog/nielsen.jpeg',
+    title: "Nielsen's 10 Usability Heuristics"
+  },
+  {
+    url: 'https://medium.com/@gerrad.natanael/scrum-or-kanban-49d767fe005a',
+    img: '/assets/blog/scrum-kanban.png',
+    title: 'Scrum or Kanban?'
+  }
+];
+
+export const projects = [
+  {
+    url: 'https://github.com/OmzetOrientedProgramming',
+    img: '/assets/project/wave.png',
+    title: 'Wave',
+    description:
+      'Final group project for Software Project course at Fasilkom UI. A web application to search and book hangout places anywhere anytime.',
+    stack: 'NextJS, TypeScript, Golang, PostgreSQL'
+  },
+  {
+    url: 'https://github.com/OmzetOrientedProgramming',
+    img: '/assets/project/forecasting-weather.png',
+    title: 'Forecasting Weather',
+    description:
+      'My friend and I created models with ARIMA, SARIMAX, and LSTM as a comparison to do forecasting weather based on data from wunderground.com.',
+    stack: 'Python'
+  },
+  {
+    url: 'https://github.com/OmzetOrientedProgramming',
+    img: '/assets/project/remindbot.png',
+    title: 'RemindBot',
+    description:
+      'Discord Bot written in Java using JDA with 3 main functionalitites namely ToDoList, Schedule, and Reminder.',
+    stack: 'Java, PostgreSQL'
+  },
+  {
+    url: 'https://github.com/OmzetOrientedProgramming',
+    img: '/assets/project/hadirin.png',
+    title: 'Hadirin',
+    description:
+      'Group project for Software Engineering course at Fasilkom UI. A web application for tracking work activities during pandemic.',
+    stack: 'Django, PostgreSQL'
+  }
+];
+
+export const navItems = [
+  {
+    name: 'HOME',
+    path: '/'
+  },
+  {
+    name: 'PROJECT',
+    path: '#project'
+  },
+  {
+    name: 'SKILL',
+    path: '#skill'
+  },
+  {
+    name: 'BLOG',
+    path: '#blog'
+  }
+];
