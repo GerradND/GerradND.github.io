72f58d48a7b18434162a9394144199beb36b98a1
gerrad.natanael <gerrad.natanael@ui.ac.id>
2022-12-08 17:22:30 +0700

    Change html semantic

--- components/Main/index.tsx
+++ components/Main/index.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 
 const Main: React.FC = () => {
   return (
-    <div className=" h-screen w-full flex flex-col lg:flex-row justify-between items-center h-full w-full pt-[1.75rem] pb-[2.5rem] md:pt-[2.5rem] lg:py-[3rem] font-['Headland_One']">
+    <section className=" h-screen w-full flex flex-col lg:flex-row justify-between items-center h-full w-full pt-[1.75rem] pb-[2.5rem] md:pt-[2.5rem] lg:py-[3rem] font-['Headland_One']">
       <div className='flex flex-col flex-1 w-full h-full justify-center'>
         <div className='max-h-full pb-10 lg:pb-16'>
           <h1 className='text-orange-cst pb-4 lg:pb-6 font-bold'>Hi,</h1>
@@ -22,7 +22,7 @@ const Main: React.FC = () => {
       <div className='h-[40vh] lg:h-[85vh] lg:pl-5 flex-1 pt-10 lg:pt-0'>
         <img className='h-full w-full object-contain' src='/assets/gerrad-pic.png' />
       </div>
-    </div>
+    </section>
   );
 };
 
--- components/Skill/index.tsx
+++ components/Skill/index.tsx
@@ -4,16 +4,16 @@ import SkillCard from './SkillCard';
 
 const Skill: React.FC = () => {
   return (
-    <div
+    <section
       id='skill'
-      className='flex flex-col h-screen w-full py-[2.5rem] sm:py-[3rem] scroll-mt-10 overflow-y-auto scrollbar'>
-      <h3 className='text-orange-cst font-bold'>SKILL</h3>
-      <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 items-center gap-5 max-h-screen  pt-[2rem] sm:pt-[3rem]'>
+      className='flex flex-col h-screen w-full py-[2.5rem] sm:py-[3rem] scroll-mt-10'>
+      <h3 className='text-orange-cst font-bold pb-[2rem] sm:pb-[3rem]'>SKILL</h3>
+      <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 items-center gap-5 max-h-screen   overflow-y-auto scrollbar'>
         {skills.map((skill) => {
           return <SkillCard url={skill.url} img={skill.img} title={skill.title} />;
         })}
       </div>
-    </div>
+    </section>
   );
 };
 export default Skill;
