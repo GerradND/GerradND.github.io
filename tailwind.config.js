84288e8375c969975fd95375cfe8c6f990e4095f
gerrad.natanael <gerrad.natanael@ui.ac.id>
2022-12-08 16:49:03 +0700

    Add new custom color theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: [
-		'./pages/**/*.{js,ts,jsx,tsx}',
-		'./components/**/*.{js,ts,jsx,tsx}',
-	],
-	theme: {
-		extend: {
-			colors: {
-				'black-1': '#000000',
-				'black-2': '#141515',
-				'orange-cst': '#FFB800',
-				'turqoise-cst': '#1FC1A4',
-			},
-		},
-	},
-	plugins: [],
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        'black-1': '#000000',
+        'black-2': '#141515',
+        'black-3': '#2F2F2F',
+        'orange-cst': '#FFB800',
+        'turqoise-cst': '#1FC1A4'
+      }
+    }
+  },
+  plugins: []
 };
