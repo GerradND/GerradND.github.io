b0cea1a95e82abd8466404cec25e123d280bcf9b
gerrad.natanael <gerrad.natanael@ui.ac.id>
2022-12-08 16:51:25 +0700

    Update grid-gap property for mobile view

--- components/Blog/index.tsx
+++ components/Blog/index.tsx
@@ -6,7 +6,7 @@ const Blog: React.FC = () => {
   return (
     <section id='blog' className='mx-auto scroll-mt-10 py-[2.5rem] sm:py-[3rem]'>
       <h3 className='text-orange-cst font-bold'>BLOGS</h3>
-      <div className='pt-[2rem] grid gap-10 grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
+      <div className='pt-[2rem] grid gap-5 md:gap-10 grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
         {articles.map((article) => {
           return (
             <Article key={article.url} url={article.url} img={article.img} title={article.title} />
