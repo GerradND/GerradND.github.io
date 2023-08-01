c358e97cd844493959765cf186933f56d483df54
GerradND <gerrad1106@gmail.com>
2023-08-01 17:55:06 +0700

    Add more styling when skill card hovered

--- components/Skill/SkillCard/index.tsx
+++ components/Skill/SkillCard/index.tsx
@@ -5,11 +5,11 @@ const SkillCard: React.FC<ArticleProps> = ({ url, img, title }) => {
   return (
     <div className='flex justify-center items-center'>
       <a className='bg-black-1' href={url}>
-        <div className='w-[100px] h-[150px] flex flex-col items-center bg-black-3 p-4 mb-2 mr-1'>
+        <div className='w-[100px] h-[150px] flex flex-col items-center bg-black-3 p-4 mb-2 mr-1 transition ease-in-out delay-100 duration-200 hover:bg-orange-cst hover:text-black-1'>
           <div className='w-[72px] h-[72px]'>
             <img className='w-full h-full object-contain' src={img} />
           </div>
-          <p className='pt-3 text-center'>{title}</p>
+          <p className='pt-3 text-center font-semibold'>{title}</p>
         </div>
       </a>
     </div>
